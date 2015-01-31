2dd62519498c51ab0aab47168a9db11b20ba8d1c
Dylan Dechant <dylan.dechant@gmail.com>
2015-01-31 12:09:12 -0500

    normal mode is done, needs to make better sense

--- lib/client.rb
+++ lib/client.rb
@@ -4,7 +4,7 @@ require "pry"
 
 class Notepassr
   include HTTParty
-  base_uri 'http://10.0.0.21:3301'
+  base_uri 'http://192.168.1.8:3301'
 
   # def initialize(user = 'apitestfun', pass = 'ironyard1')
   #   @auth = {:username => user, :password => pass}
@@ -39,8 +39,11 @@ class Notepassr
     message = gets.chomp
     print "What is your id: "
     sender_id = gets.chomp
-    options = {:recipient_id => recipient_id, :title => title, :content => message, :sender_id => sender_id}
+    print "What is your password: "
+    password = gets.chomp
+    options = {:recipient_id => recipient_id, :title => title, :content => message, :sender_id => sender_id, :password => password}
     resp = self.class.post("/message", :body => options)
+    binding.pry
   end
 
   def get_all_users
@@ -53,6 +56,30 @@ class Notepassr
     #binding.pry
   end
 
+  def get_single_user
+    print "What user would you like to see: "
+    user = gets.chomp.to_i
+    resp = self.class.get("/user/#{user}")
+    user_profile = JSON.parse(resp)
+    puts user_profile
+    gets
+  end
+
+  def delete_user
+    print "What user would you like to delete: "
+    d_user = gets.chomp.to_i
+    resp = self.class.delete("/user/#{d_user}")
+    binding.pry
+  end
+
+  def get_all_messages
+    system 'clear'
+    resp = self.class.get("/message")
+    message = JSON.parse(resp.body)
+    puts message
+    gets
+  end
+
 exit = 0
 system 'clear'
 puts "Notepassr Client"
@@ -63,6 +90,8 @@ while exit != 99
   puts "2) Send message"
   puts "3) Get 1 user"
   puts "4) Get all users"
+  puts "5) Delete a user"
+  puts "6) Get all messages"
   puts "99) Exit"
   exit = gets.chomp.to_i
   case exit
@@ -70,8 +99,14 @@ while exit != 99
     client.create_user
   when 2
     client.send_message
+  when 3
+    client.get_single_user
   when 4
     client.get_all_users
+  when 5
+    client.delete_user
+  when 6
+    client.get_all_messages
   end
 end
 end
--- lib/notepasser.rb
+++ lib/notepasser.rb
@@ -7,16 +7,19 @@ Camping.goes :Notepasser
 
 module Notepasser
 
+
+
+end
+
+module Notepasser::Models
+
     class User < ActiveRecord::Base
       has_many :messages
     end
 
     class Message < ActiveRecord::Base
+      belongs_to :users
     end
-
-end
-
-module Notepasser::Models
 end
 
 
@@ -34,29 +37,88 @@ module Notepasser::Controllers
     end
   end
 
-  class UserController < R '/user/new'
+  class UserController < R '/user'
 
+    # CREATE A NEW USER
+    def post
+      @input.symbolize_keys!
+      new_user = User.new
+      [:name, :password].each do |x|
+        new_user[x] = @input[x]
+      end
+      new_user.save
+      @status = 201
+      {:message => "New user created",
+       :code => 201}
+    end
+
+    # GET ALL USERS
     def get
-      User.create(name: "test")
-      "trying to create a new user, eh?"
+      all_users = {}
+      all_users = User.all.to_json
+      "#{all_users}"
     end
 
   end
 
+  #Individual Users
   class UsersController < R '/user/(\d+)'
 
     def get(id)
-       db = User.find(id)
-      "#{db.name}"
+      @input.symbolize_keys!
+      db = User.where(id).to_json
+      "#{db}"
+    end
+
+    def delete(id)
+      binding.pry # if record doesn't exist you get this: ActiveRecord::RecordNotFound
+      begin
+        delete = User.destroy(id)
+        "User #{id} deleted"
+      rescue ActiveRecord::RecordNotFound
+        @status = 404
+      end
     end
   end
 
-  # class UserController < R '/user/new'
+  class MessageController < R '/message'
 
-  #   def get
-  #     Notepasser::User.create_user
-  #   end
+    def post
+      @input.symbolize_keys!
+      t = User.where(id: @input[:sender_id]).take
+      pass = t[:password]
+      if pass == @input[:password]  #AUTHENTICATE PASSWORD
+        new_message = Message.new
+        [:recipient_id, :title, :content, :sender_id].each do |x|
+          new_message[x] = @input[x]
+        end
+        new_message.save
+        @status = 201
+        "message sent"
+      else
+        @status = 401
+        {:message => "Incorrect password",
+          :code => 401}
+          "Incorrect Password"
+      end
+      # binding.pry
+    end
 
-  # end
+    def get
+       message = Message.all.to_json
+       "#{message}"
+     end
+  end
+
+  class IndividualMessage < R '/message/(\d+)'
+    def get(user_id)
+      message = Message.where(recipient_id: user_id)
+      message.each do |x|
+        x.update(read: true)
+        x.save
+      end
+      "#{message.to_json}"
+    end
+  end
 
 end
